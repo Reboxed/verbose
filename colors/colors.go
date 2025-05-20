aff8f2fc1833c5690cdf9c19f5e9c32f2b5526a8
FoxTale-Labs <72300200+FoxTale-Labs@users.noreply.github.com>
2025-05-20 14:49:10 -0700

    Delete colors/colors.go

--- colors/colors.go
+++
@@ -1,9 +0,0 @@
-package colors
-
-import "github.com/fatih/color"
-
-var Error = color.New(color.BgHiRed, color.FgHiWhite)
-var Info = color.New(color.BgHiBlue, color.FgHiWhite)
-var Warn = color.New(color.BgHiYellow, color.FgHiWhite)
-var Success = color.New(color.FgBlack).AddBgRGB(39, 196, 39)
-var Neutral = color.New(color.BgWhite, color.FgHiWhite)
