39ef4ce277de014b3af3690292e3741ba8b53a1b
Mart <93423789+SwiftDust@users.noreply.github.com>
2025-05-22 15:33:15 +0000

    Change file name and some other changes

--- constants.go
+++ constants.go
@@ -7,3 +7,9 @@ var InfoColor = color.New(color.BgHiBlue, color.FgHiWhite)
 var WarnColor = color.New(color.BgHiYellow, color.FgHiWhite)
 var SuccessColor = color.New(color.FgBlack).AddBgRGB(39, 196, 39)
 var NeutralColor = color.New(color.BgWhite, color.FgHiWhite)
+
+var Error = "ERR"
+var Info = "INFO"
+var Warn = "WARN"
+var Success = "OK"
+var Print = "PRINT"
